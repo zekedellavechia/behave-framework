6c241ad863377d8859b7a0fd3e18c12033f18727
zekedellavechia <ezequiel.dellavechia@gmail.com>
2020-12-27 21:01:18 -0300

    Adding Gecko path

--- features/environment.py
+++ features/environment.py
@@ -2,5 +2,8 @@ from selenium import webdriver
 
 
 def before_scenario(context, scenario):
-    context.driver = webdriver.Chrome()
+    # context.driver = webdriver.Chrome()
+    # context.driver.maximize_window()
+
+    context.driver = webdriver.Firefox()
     context.driver.maximize_window()
